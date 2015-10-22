3da7aaa3c1ff1a19d37e97ffb7f9548f940792e4
qitang <qtang@acupay.com>
2015-10-22 15:45:43 -0400

    added test cases

--- test/index.js
+++ test/index.js
@@ -1,18 +1,78 @@
 var BinaryHeap = require('../lib/heap.js')
 
-
  
-var assert = require('assert');
+var assert = require('chai').assert;
 
-console.log(assert)
 
-describe('Array', function() {
-	var heap;
+describe('min heap', function() {
+	var heap
 	beforeEach(function() {
-	    heap = new BinaryHeap();
+	     heap = new BinaryHeap();
 	});
 
-	it('should return undefined when the heap is empty', function () {
-	  assert.isUndefined(heap.pop(), 'no element');
+	it('peak should return undefined when the queue is empty', function () {
+	  assert.isUndefined(heap.peak());
 	});
+
+	it('peak should return minimium number', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(-43 , heap.peak())
+	});
+
+	it('pop should remove minimium number', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(-43 , heap.pop())
+	   assert.equal(-33 , heap.pop())
+	});
+
+	it('size should return number of elements', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(6 , heap.size())
+	});
+
+});
+
+
+describe('max heap', function() {
+	var heap
+	beforeEach(function() {
+	     heap = new BinaryHeap(function(a,b){
+	     	if(a > b) return -1;
+	     	if(a < b) return 1;
+	     	return 0;
+	     });
+	});
+
+	it('peak should return undefined when the queue is empty', function () {
+	  assert.isUndefined(heap.peak());
+	});
+
+	it('peak should return minimium number', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(50 , heap.peak())
+	});
+
+	it('pop should remove maximium number', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(50 , heap.pop())
+	   assert.equal(43 , heap.pop())
+	});
+
+	it('size should return number of elements', function () {
+	   [2,43,50,-43,-33, -1].forEach(function(v){
+	   		heap.push(v)
+	   })
+	   assert.equal(6 , heap.size())
+	});
+
 });
